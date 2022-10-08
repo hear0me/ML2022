df0056caac379af8746dc309a6c085f505e1aab1
hear0me <3178476604@qq.com>
2022-10-08 16:27:23 +0800

    add some auto created file

--- hw1/HW1.py
+++ hw1/HW1.py
@@ -226,6 +226,7 @@ def save_pred(preds, file):
         for i, p in enumerate(preds):
             writer.writerow([i, p])
 
+
 model = My_Model(input_dim=x_train.shape[1]).to(device)
 model.load_state_dict(torch.load(config['save_path']))
 preds = predict(test_loader, model, device)
---
+++ hw1/test.py
@@ -0,0 +1,5 @@
+import math
+
+allNum = list(range(0, 200))
+b = allNum[0:50] + allNum[53:66] + allNum[69:82] + allNum[85:98] + allNum[101:114] + allNum[117:118]
+print(allNum)
